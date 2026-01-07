1617ab287c38b0a017ffb8b207b9fc94c6e49b22
Theodor Wulff <theodor.wulff@manchester.ac.uk>
2026-01-07 10:43:23 +0000

    added prompt perturbation

---
+++ method/prompts/perturbator.py
@@ -0,0 +1,36 @@
+class PerturbedPromptOutput:
+    """
+    Docstring for PerturbedPromptOutput
+
+    Class to handle the output of perturbed prompts.
+    """
+    def __init__(self, original_prompt, perturbed_prompt):
+        self.original_prompt = original_prompt
+        self.perturbed_prompt = perturbed_prompt
+
+
+class PromptPerturbator:
+    """
+    Docstring for PromptPerturbator
+
+    Class to handle perturbation of prompts based on given configuration and method calls.
+    """
+    def __init__(self, config):
+        self.config = config
+
+    def directional_perturbation(self, prompt):
+        if "left" in prompt:
+            perturbed_prompt = prompt.replace("left", "right")
+        elif "right" in prompt:
+            perturbed_prompt = prompt.replace("right", "left")
+        elif "up" in prompt:
+            perturbed_prompt = prompt.replace("up", "down")
+        elif "down" in prompt:
+            perturbed_prompt = prompt.replace("down", "up")
+        else:
+            perturbed_prompt = prompt
+        return PerturbedPromptOutput(prompt, perturbed_prompt)
+
+    def perturb(self, data):
+        # Implement perturbation logic based on config
+        pass
