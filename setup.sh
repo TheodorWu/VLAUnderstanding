d3a0ce97fd149e305771fcd7d87870a9cbf028b9
Theodor Wulff <theodor.wulff@manchester.ac.uk>
2026-01-21 11:17:56 +0000

    setup scripts

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Create virtual environment
+echo "Creating virtual environment..."
 python3 -m venv .venv
 
 # Activate virtual environment
+echo "Activating virtual environment..."
 source .venv/bin/activate
 
 # Install dependencies
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
 # Run all unit tests in test folder
+echo "Running unit tests..."
 python -m unittest discover -s tests -p "test_*.py"
+
+echo "Setup complete!"
